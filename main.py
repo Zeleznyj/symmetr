c6bf864ece41047f19cfe0d4a87103a2fc918c7d
Jakub Zelezny <jakub.zelezny@gmail.com>
2014-12-17 12:03:31 +0100

    Rewritten using symbolic toolbox
    Rewritten using sympy, now in symmetrize_sympy.py
    also started adding the analysis of the effect of symmetry on each sublattice - in main.py

--- main.py
+++ main.py
@@ -1,8 +1,80 @@
-import symmetrize
+import re
+import sys
+
+import symmetrize_sympy
 import mat
+
+import sympy
+from scipy import linalg
+import math
+
+
+def transform_position(pos,sym):
+
+  #transforms the position
+  transd = []
+  for i in range(3): #loop over components of position
+    op = re.sub('-','+-',sym[1][i])
+    transd.append(0)
+    op = re.split('\+',op) #op contains a list of all operations we have to do with component
+    op = filter(None,op) #remove empty strings from the list  
+    for j in range(len(op)):
+      if re.match('x',op[j]):
+        transd[i] = transd[i] + float(pos[0])
+      if re.match('-x',op[j]):
+        transd[i] = transd[i] - float(pos[0])
+      if re.match('y',op[j]):
+        transd[i] = transd[i] + float(pos[1])
+      if re.match('-y',op[j]):
+        transd[i] = transd[i] - float(pos[1])
+      if re.match('z',op[j]):
+        transd[i] = transd[i] + float(pos[2])
+      if re.match('-z',op[j]):
+        transd[i] = transd[i] - float(pos[2])
+      if re.match('-?[0-9]*[./]?[0-9]+',op[j]):
+        if re.match('-?[0-9.]+/[0-9.]+',op[j]):
+          op_s = op[j].split('/')
+          op[j] = float(op_s[0]) / float(op_s[1])
+        transd[i] = transd[i] + float(op[j])
+
+  for i in range(3):
+    if math.floor(transd[i]) != 0:
+      transd[i] = transd[i] - math.floor(transd[i])
+
+  #transforms the momentum
+  transd_m = []
+  for i in range(3): #loop over components of magnetic moment
+    op = re.sub('-','+-',sym[2][i])
+    transd_m.append(0)
+    op = re.split('\+',op) #op contains a list of all operations we have to do with component
+    op = filter(None,op) #remove empty strings from the list  
+    for j in range(len(op)):
+      if re.match('mx',op[j]):
+        transd_m[i] = transd_m[i] + float(pos[3])
+      if re.match('-mx',op[j]):
+        transd_m[i] = transd_m[i] - float(pos[3])
+      if re.match('my',op[j]):
+        transd_m[i] = transd_m[i] + float(pos[4])
+      if re.match('-my',op[j]):
+        transd_m[i] = transd_m[i] - float(pos[4])
+      if re.match('mz',op[j]):
+        transd_m[i] = transd_m[i] + float(pos[5])
+      if re.match('-mz',op[j]):
+        transd_m[i] = transd_m[i] - float(pos[5])
+      if re.match('-?[0-9]*[./]?[0-9]+',op[j]):
+        if re.match('-?[0-9.]+/[0-9.]+',op[j]):
+          op_s = op[j].split('/')
+          op[j] = float(op_s[0]) / float(op_s[1])
+        transd_m[i] = transd_m[i] + float(op[j])
+
+  return list(transd+transd_m)
+
+
 
 data = open('sym.out')
 lines = data.readlines()
+
+#read basis transformation
 for i in range(len(lines)):
   if "Vectors a,b,c:" in lines[i]:
     pos_vectors = i
@@ -21,14 +93,107 @@ print 'b = ', vec_b[0], '* x + ', vec_b[1], '* y + ', vec_b[2], '* z'
 print 'c = ', vec_c[0], '* x + ', vec_c[1], '* y + ', vec_c[2], '* z'
 print ''
 
-symmetries = open('sym.dat')
 
-matrix = symmetrize.symmetr(symmetries)
+
+#read atomic positions
+#format: list containing six floats first three are the positions the other three magnetic moment
+for i in range(len(lines)):
+  if "Atomic positions and magnetic moments in terms of a,b,c:" in lines[i]:
+    pos_pos = i
+
+cont = 1
+positions = []
+i = 0
+while cont == 1:
+  i = i+1
+  if re.match('\A +[0-9]+ ',lines[pos_pos + i]):
+    positions.append(lines[pos_pos + i])
+  elif re.match('-------',lines[pos_pos + i]):
+    cont = 0
+
+for i in range(len(positions)):
+  positions[i] = positions[i].split()
+  positions[i].pop(0)
+  for l in range(len(positions[i])):
+    positions[i][l] = float(positions[i][l])
+
+
+
+#read symmetries 
+for i in range(len(lines)):
+  if "_space_group_symop.magn_operation_mxmymz" in lines[i]:
+    pos_syms = i
+
+cont = 1
+syms = []
+i = 0
+while cont == 1:
+  i = i+1
+  if re.match('\A[0-9]+ ',lines[pos_syms + i]):
+    syms.append(lines[pos_syms + i])
+  else:
+    cont = 0
+
+for i in range(len(syms)):
+  syms[i] = syms[i].split()
+  syms[i][1] = syms[i][1].split(',')
+  syms[i][2] = syms[i][2].split(',')
+  syms[i].append(syms[i][1][3])
+  syms[i][1] = syms[i][1][0:3]
+
+print syms
+for i in range(len(positions)):
+  for l in range(len(syms)):
+    sym_trans = []
+    trans = transform_position(positions[i],syms[l])
+    for j in range(len(positions)):
+      if trans == positions[j]:
+        sym_trans.append((i,j))
+    if not sym_trans:
+      print positions[i]
+      print trans
+      print syms[l]
+    syms[l].append(list(sym_trans))
+
+
+print syms
+    
+
+
+
+
+sys.exit('I\' too old for this shit')
+
+matrix = symmetrize_sympy.symmetr(syms)
 
 print 'Symmetrized matrix in the abc basis intraband term:'
-mat.print_matrix2(matrix[0])
+print matrix[0]
 print 'Symmetrized matrix in the abc basis interband term:'
-mat.print_matrix2(matrix[1])
+print matrix[1]
 
+#transformation matrix:
 
+T = sympy.zeros(3,3)
 
+T[0,0] = vec_a[0]
+T[1,0] = vec_b[0]
+T[2,0] = vec_c[0]
+
+T[0,1] = vec_a[1]
+T[1,1] = vec_b[1]
+T[2,1] = vec_c[1]
+
+T[0,2] = vec_a[2]
+T[1,2] = vec_b[2]
+T[2,2] = vec_c[2]
+
+Ti = sympy.Matrix(linalg.inv(T))
+
+matrix_T = []
+matrix_T.append(Ti*matrix[0]*T)
+matrix_T.append(Ti*matrix[1]*T)
+
+print 'Symmetrized matrix in the original basis intraband term:'
+print matrix_T[0]
+print 'Symmetrized matrix in the original basis interband term:'
+print matrix_T[1]
---
+++ symmetrize_sympy.py
@@ -0,0 +1,191 @@
+import re
+import copy
+import mat
+import sympy
+import sys
+
+#matrices 3x3 are stored in the following 1d form
+#note that python starts numbering from 0, so the following represents a matrix:
+# 00 01 02
+# 10 11 12
+# 20 21 22
+#matrix = ['00','01','02','10','11','12','20','21','22']
+
+def convert_space(s):
+  if re.match('x((\+|-)[0-9]+(\.[0-9]+)*/[0-9]+(\.[0-9]+)*)*\Z',s):
+    index = 0
+    minus = 1
+  if re.match('-x((\+|-)[0-9]+(\.[0-9]+)*/[0-9]+(\.[0-9]+)*)*\Z',s):
+    index = 0
+    minus = -1
+  if re.match('y((\+|-)[0-9]+(\.[0-9]+)*/[0-9]+(\.[0-9]+)*)*\Z',s):
+    index = 1
+    minus = 1
+  if re.match('-y((\+|-)[0-9]+(\.[0-9]+)*/[0-9]+(\.[0-9]+)*)*\Z',s):
+    index = 1
+    minus = -1
+  if re.match('z((\+|-)[0-9]+(\.[0-9]+)*/[0-9]+(\.[0-9]+)*)*\Z',s):
+    index = 2
+    minus = 1
+  if re.match('-z((\+|-)[0-9]+(\.[0-9]+)*/[0-9]+(\.[0-9]+)*)*\Z',s):
+    index = 2
+    minus = -1
+
+  return [index,minus]
+
+def convert_spin(s):
+  if s == 'mx':
+    index = 0
+    minus = 1
+  if s == '-mx':
+    index = 0
+    minus = -1
+  if s == 'my':
+    index = 1
+    minus = 1
+  if s == '-my':
+    index = 1
+    minus = -1
+  if s == 'mz':
+    index = 2
+    minus = 1
+  if s == '-mz':
+    index = 2
+    minus = -1
+  return [index,minus]
+
+
+def update_trans_current(matrix_current,matrix_trans):
+
+  trans_current = sympy.zeros(3,3)
+
+  for i in range(3):
+    for j in range(3):
+      n = mat.convert_index(i,j)
+
+      trans_current[i,j] = matrix_trans[n][2] * matrix_current[matrix_trans[n][0],matrix_trans[n][1]]
+
+  return trans_current
+
+
+#returns a symmetrical form of a spin-orbit torque response matrix given list of symmetries
+def symmetr(symmetries):
+
+  #this defines starting response matrix
+  #we repeat it twice, once for intraband term and once for the interband term
+  Xo = sympy.Matrix(sympy.MatrixSymbol('Xo',3,3))
+  Xx = sympy.Matrix(sympy.MatrixSymbol('Xx',3,3))
+  X = []
+  X.append(Xo)
+  X.append(Xx)
+
+  #we do a loop over all symmetry, for each symmetry, we find what form the response matrix can have, when the system has this symmetry
+  #for next symmetry we take the symmetrized matrix from the previous symmetry as a starting point
+  for sym in symmetries:
+
+    #this just splits the symmetry information into more practical form
+    sym_split = sym.split()
+    #this is a type of information, so far A means consider the operation and anything else means skip it
+    op_type=sym_split[1]
+    sym_split2 = sym_split[2].split(',')
+    #space transformation: 
+    sym1 = sym_split2[0:3]
+    #-1 if the symmetry contains time reversal, 1 otherwise
+    time_reversal = sym_split2[3]
+    #spin transformation
+    sym2 = sym_split[3].split(',')
+
+    if op_type == 'A':
+      #print 'Symmetry operation: ', sym_split
+      #we do everything separately for the intra and inter band terms
+      #most things are the same, the only difference in the physics is that when time-reversal is present, interband transformation has
+      #minus compared to the intraband transformation
+      for l in range(2):
+        #for all of the components of the matrix we look at how they will be transformed by the symmetry operation
+
+        matrix_trans = []
+
+        for n in range(9):
+
+          index = mat.inconvert_index(n)
+          v_trans = convert_space(sym1[index[1]])
+          s_trans = convert_spin(sym2[index[0]])
+
+          #if there is a time-reversal symmetry present, v will will have additional minus compared to space transformation
+          if time_reversal == '-1':
+            v_trans[1] = -1 * v_trans[1]
+          
+          comp_trans = [0,0,0]
+          comp_trans[0] = s_trans[0]
+          comp_trans[1] = v_trans[0]
+          if v_trans[1] * s_trans[1] > 0:
+            comp_trans[2] = 1
+          if v_trans[1] * s_trans[1] < 0:
+            comp_trans[2] = -1
+
+
+          #if time-reversal is present, interband has additional minus
+          if l == 1:
+            if time_reversal == '-1':
+              comp_trans[2] = -1 * comp_trans[2]
+
+          #matrix_trans is the transformed matrix - for example, if component 00 of matrix_trans is 11 it means the 00 component of the response matrix transforms to the 11 component
+          #the last number is the sign
+          matrix_trans.append(list(comp_trans))
+
+        #the response matrix can have a form that is already constrained from a previous symmetry operation, we take this into consideration
+        #and store the information in matrix_trans_current
+        #for example under symmetry operation R, chi_00, may transform to chi_11, but we may already know that chi_11 must be equal to
+        #-chi_00, then matrix_current[0] will be 00, matrix_current[4]=-00, matrix_trans[0]=11 and matrix_trans_current[0]=-00
+        matrix_trans_current = update_trans_current(X[l],matrix_trans)
+
+        #if l == 0:
+        #  print 'Transformed matrix intraband term'
+        #else:
+        #  print 'Transformed matrix interband term'
+        #print_matrix(matrix_trans)
+
+
+        #now we go through the response matrix and look at what must hold for it due to symmetry
+        #only simple rules are implemented, but these should be sufficient if no hexagonal symmetries are present
+        #if there are hexagonal symmetries, the program breaks anyway
+        #we look at transformation and modify the response matrix according to the rules, if something changes we look at the transformation
+        # again and we repeat until nothing changes
+
+        changed = 1
+
+        while changed == 1:
+
+          changed = 0
+
+          for i in range(3):
+            for j in range(3):
+
+              n = mat.convert_index(i,j)
+
+              #if the transformed component is the same we do nothing
+                #if they only differ in sign, the component must be zero
+              if X[l][i,j] != 0 and matrix_trans_current[i,j] !=0:
+
+                if sympy.simplify(X[l][i,j]+matrix_trans_current[i,j]) == 0:
+                  X[l][i,j] = 0
+                  changed = 1
+                elif mat.convert_index(matrix_trans[n][0],matrix_trans[n][1]) < n and sympy.simplify(X[l][i,j]-matrix_trans_current[i,j]) != 0:
+                  #if the component is equal to another component we set it equal to that
+                  #however if we did that for each component, we wouldn't get any information
+                  #therefore if two components are equal we keep the one that ha lower index in the 1d matrix form and the  other one set equal
+                  #to the first one
+                  #index_current = mat.convert_index(matrix_current[l][n][0],matrix_current[l][n][1])
+                  #index_current = n
+                  changed = 1
+                  X[l][i,j] = matrix_trans_current[i,j]
+
+            #since the response matrix may have changed we also have to modify the matrix matrix_trans_current
+          matrix_trans_current = update_trans_current(X[l],matrix_trans)
+
+  #sys.exit('kaslu na to')
+
+  return X
+    
+
+  
