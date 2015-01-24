ce334f89f070cbb723f89bbc5e81f4f5deb73cbf
Jakub Zelezny <jakub.zelezny@gmail.com>
2015-01-24 18:25:32 +0100

    added some comments

--- tensors.py
+++ tensors.py
@@ -2,6 +2,24 @@ import sympy
 import copy
 
 class tensor:
+  #this creates a symbolic tensor
+  #use by t=tensor('s',dim1,dim2), where 's' means the tensor should be symbolic, dim2 is the number of indeces
+  # of the tensor, dim1 is the number of values each index can have
+  #by default variables will be called x### for a different variable name, run with tensor('s',dim1,dim2,'name')
+  #for nonsymbolic zero tensor, run  tensor(0,dim1,dim2)
+  #it's possible to:
+  #print by print t
+  #write out element of the tensor by t[1,2,3] or t[(1,2,3)] for example
+  #assign a value to element: t[1,2,3] = 0
+  #sum or substract two tensors: t1+t2
+  #multiply by a number or symbolic variable: t*2
+  #loop over all elements, following will print all elements for example
+  # for i in t:
+  #   print t[i]
+  #if you need to use the symbolic variables use t.x[index], for example for x011, use t.x[0,1,1] (or t.x[(0,1,1)])
+  #you can do for example t[0,0,0] = t.x[1,1,1], then t[0,0,0] will be equal to x111
+  #you can also access x011 by t['x011']
+
 
   def __init__(self,kind,dim1,dim2,name='x'):
 
@@ -18,7 +36,9 @@ class tensor:
         s_tot = var_name(name,ind)
         self.v[s_tot] = sympy.symbols(s_tot)
 
+    #self.t contains the tensor itself, stored as a dictionary
     self.t = {}
+    #self.x contains the symbolic variables in a form that can be easily retrieved
     self.x = {}
     for ind in self.inds:
       if kind == 0:
