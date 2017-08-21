53d065ea600f194b35a3b3645d2ab7ab0589666a
Jakub Zelezny <jakub.zelezny@gmail.com>
2017-08-21 20:47:40 +0200

    Added a licence header to all files

--- symmetr/conv_index.py
+++ symmetr/conv_index.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 def convert_index(i,j):
     matrix = ['00','01','02','10','11','12','20','21','22']
     n = i*3+j
